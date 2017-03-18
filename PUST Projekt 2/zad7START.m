b4b684fb55e123bfb40f434fc8e14bb171b2a378
Antoni Rozanski <ant.rozanski@gmail.com>
2017-03-18 22:33:36 +0100

    Uaktualnione zad7START

--- PUST Projekt 2/zad7START.m
+++ PUST Projekt 2/zad7START.m
@@ -1,7 +1,11 @@
-DZ=60;
-N=30;
-Nu=10;
-lambda=0.1;
+%Nastawy, z którymi zostanie uruchomiony algorytm DMC
+DZ=40;
+N=8;
+Nu=1;
+lambda=0.3;
+
+%intensywnoœæ szumów w torze zak³óceñ
 Z=0.1;
+
 [s,sz]=zad3(false,false);
 zad7(DZ,N,Nu,lambda,s,sz,true,false,Z)
