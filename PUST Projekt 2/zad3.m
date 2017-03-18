6c302675395877b50b15047cf15e290351402e84
Antoni Rozanski <ant.rozanski@gmail.com>
2017-03-18 18:30:54 +0100

    zad3 - matlab + latex

--- PUST Projekt 2/zad3.m
+++ PUST Projekt 2/zad3.m
@@ -1,10 +1,11 @@
-function [ s,sz ] = zad3(Latex,plot)
-% wyznacza odpowiedzo skokowe i normalizuje 
+function [ s,sz ] = zad3(Latex,plot_arg)
+% wyznacza odpowiedzi skokowe i normalizuje 
 kk=200;
 Tp=0.5;
 dU=1;
 dZ=1;
 startk=15;
+
 %-------------------------------U----------------------------------------
 U(1:kk)=0;
 Y(1:kk)=0;
@@ -14,7 +15,7 @@ for k=startk:kk
     Y(k)=symulacja_obiektu7y(U(k-4),U(k-5),Z(k-1),Z(k-2),Y(k-1),Y(k-2));
 end
 s=Y(startk+1:kk);
-if(plot)
+if(plot_arg)
     figure(1)
     plot(s);
     title('Znormalizowana odp. skokowa dla toru u')
@@ -24,6 +25,7 @@ end
 if(Latex)
     toPlotForLatex('z3s',1:kk-startk,s)
 end
+
 %------------------------------Z----------------------------------------
 U(1:kk)=0;
 Y(1:kk)=0;
@@ -34,14 +36,16 @@ for k=startk:kk
 end
 
 sz=Y(startk+1:kk);
-if(plot)
+if(plot_arg)
     figure(2)
     plot(sz);
     title('Znormalizowana odp. skokowa dla toru z')
     xlabel('sz')
     ylabel('k')
 end
+
 if(Latex)
     toPlotForLatex('z3sz',1:kk-startk,sz)
 end
+
 end
