c71bf060435f9eff2dd9317c121275cc9bd9bbbc
Antoni Rozanski <ant.rozanski@gmail.com>
2017-03-18 16:54:02 +0100

    zad1 - matlab + sprawko

--- PUST Projekt 2/zad1.m
+++ PUST Projekt 2/zad1.m
@@ -1,17 +1,29 @@
 clear all;
 
-Tp=0.5;
-Upp=0;
-U = zeros(200,1);
-Y = zeros(200,1);
-Z = zeros(200,1);
-for k=5:200
-    Y(k)=symulacja_obiektu7y(Upp,Upp,Z(k-1),Z(k-2),Y(k-1),Y(k-2));
-end  %U(k-4),U(k-5)
 
+%inicjalizacja wektorów
+U = zeros(kk,1);
+Y = zeros(kk,1);
+Z = zeros(kk,1);
+
+%czas symulacji
+kk=200;
+
+for k=6:kk
+    Y(k)=symulacja_obiektu7y(U(k-4),U(k-5),Z(k-1),Z(k-2),Y(k-1),Y(k-2));
+end
+
+%wykresy
 plot(Y);
+hold on;
+plot(U);
+plot(Z);
 ylabel('y');
 xlabel('kroki');
-legend('Y(k)');
-title('Punkt pracy Upp = 1');
-toPlotForLatex('z1',1:length(Y),Y');
+legend('Y(k)','U(k)','Z(k)');
+title('Punkt pracy: Upp = Ypp = Zpp = 0');
+
+%zapis wykresów do Latexa
+toPlotForLatex('z1u',1:length(U),U');
+toPlotForLatex('z1y',1:length(Y),Y');
+toPlotForLatex('z1z',1:length(Z),Z');
