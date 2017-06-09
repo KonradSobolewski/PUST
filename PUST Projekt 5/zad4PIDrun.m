f30c1ed7a91407feb615ab3971d84fc799edcbca
Dangield <Dan.gield@gmail.com>
2017-06-09 13:44:50 +0200

    Pierwsza konfiguracja PID zakończona

--- PUST Projekt 5/zad4PIDrun.m
+++ PUST Projekt 5/zad4PIDrun.m
@@ -1,8 +1,8 @@
-K=[0 0 0];
-Ti=[Inf Inf Inf];
+K=[6.6821 9.0985/2 5.2295/2];
+Ti=[3 5 3];
 Td=[0 0 0];
 var=[3 2 4];
 draw=true;
 latex=false;
-% latex=true;
+latex=true;
 zad3PID(K,Ti,Td,var,draw,latex)
